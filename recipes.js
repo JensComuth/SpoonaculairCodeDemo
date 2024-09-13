618b3c38436c1e911638604971e6f272d0c63bbc
Jens Comuth <131358917+JensComuth@users.noreply.github.com>
2024-09-13 11:18:24 +0200

    Api-keys veranderd

--- recipes.js
+++ recipes.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('#searchButton').on('click', function () {
         $.ajax({
-            url: `https://api.spoonacular.com/recipes/complexSearch?query=${$('#searchValue').val()}&apiKey=c25e2d349ba842ee8186ded1ff30b942`
+            url: `https://api.spoonacular.com/recipes/complexSearch?query=${$('#searchValue').val()}&apiKey=939298d11944417487967a60ba791bf5`
         }).done(function (data) {
             data.results.forEach(recipe => {
                 const searchResults = document.querySelector('.searchResults');
