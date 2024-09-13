d11641a1c8da0a655276ea5fac21a671f8bf40ac
Jens Comuth <131358917+JensComuth@users.noreply.github.com>
2024-09-13 11:19:48 +0200

    Reset bug fixed

--- recipes.js
+++ recipes.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
     $('#searchButton').on('click', function () {
+        const searchResults = document.querySelector('.searchResults');
+        searchResults.innerHTML = "";
         $.ajax({
             url: `https://api.spoonacular.com/recipes/complexSearch?query=${$('#searchValue').val()}&apiKey=939298d11944417487967a60ba791bf5`
         }).done(function (data) {
             data.results.forEach(recipe => {
-                const searchResults = document.querySelector('.searchResults');
 
                 const newItem = document.createElement('div');
                 newItem.classList.add('recipe');
